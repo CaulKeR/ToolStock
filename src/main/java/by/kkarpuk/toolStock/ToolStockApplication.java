4d91279bae1bd0abd37c9846694d9adcf58b9467
kkarpuk <kirillkarpuk0@gmail.com>
2020-03-15 15:33:02 +0300

    Initial commit

---
+++ src/main/java/by/kkarpuk/toolStock/ToolStockApplication.java
@@ -0,0 +1,13 @@
+package by.kkarpuk.toolStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kkarpuk/toolStock/ToolStockApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kkarpuk.toolStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
